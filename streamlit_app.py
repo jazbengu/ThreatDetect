a0d708f712fc5cb09ea00c39875c01d96fa8de43
Joy Azile Zoe Bengu <111288977+jazbengu@users.noreply.github.com>
2026-05-02 15:30:08 +0000

    created main page, added file uploader

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,31 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+def main():
+    st.title("ThreatFind")
+    st.write("snuff out any potential insider thereats \n\n\n\n")
+
+    page = st.sidebar.selectbox(
+    "Choose a page:",
+    ["Organisational Search via CSV", "Single Search", "Exploratory Data Analaysis"]
 )
+
+    if page == "Organisational Search via CSV":
+        st.title("Dashboard")
+        # add line for enter csv file and prompt
+        st.write("Please Enter Your CSV File For Analysis")
+        file_upload = st.file_uploader("Select Your File", type="csv")
+
+
+    elif page == "Single Search":
+        st.title("Single Search")
+        # Chart code here
+
+    elif page == "Exploratory Data Analaysis":
+        st.title("EDA")
+        # Settings code here
+
+   
+    
+if __name__=="__main__":
+    main()
+
